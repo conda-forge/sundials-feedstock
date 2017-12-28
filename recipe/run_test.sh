c527eb275876229260fddd8b120049076996c06f
Björn Dahlgren <bjodah@gmail.com>
2017-12-28 22:55:34 +0100

    Do not test OpenMP case on OS X

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -4,14 +4,16 @@ echo "cmake_minimum_required(VERSION 2.8)" > CMakeLists.txt
 echo "project (my_sundials_test)" >> CMakeLists.txt
 echo "add_executable (cvRoberts_dns cvRoberts_dns.c)" >>CMakeLists.txt
 echo "add_executable (cvAdvDiff_bndL cvAdvDiff_bndL.c)" >>CMakeLists.txt
-echo "add_executable (cvAdvDiff_bnd_omp cvAdvDiff_bnd_omp.c)" >>CMakeLists.txt
 echo "target_link_libraries (cvRoberts_dns LINK_PUBLIC sundials_cvode sundials_nvecserial)" >>CMakeLists.txt
 echo "target_link_libraries (cvAdvDiff_bndL LINK_PUBLIC sundials_cvode sundials_nvecserial sundials_sunlinsollapackband openblas)" >>CMakeLists.txt
-echo "target_link_libraries (cvAdvDiff_bnd_omp LINK_PUBLIC sundials_cvode sundials_nvecopenmp)" >>CMakeLists.txt
 echo "enable_testing()" >>CMakeLists.txt
 echo "add_test (test_roberts cvRoberts_dns)" >>CMakeLists.txt
 echo "add_test (test_advdiff_L cvAdvDiff_bndL)" >>CMakeLists.txt
-echo "add_test (test_advdiff_omp cvAdvDiff_bnd_omp)" >>CMakeLists.txt
+if [ $(uname -s) != 'Darwin' ]; then
+    echo "add_executable (cvAdvDiff_bnd_omp cvAdvDiff_bnd_omp.c)" >>CMakeLists.txt
+    echo "target_link_libraries (cvAdvDiff_bnd_omp LINK_PUBLIC sundials_cvode sundials_nvecopenmp)" >>CMakeLists.txt
+    echo "add_test (test_advdiff_omp cvAdvDiff_bnd_omp)" >>CMakeLists.txt
+fi
 
 cmake .
 C_INCLUDE_PATH=$PREFIX/include LIBRARY_PATH=$PREFIX/lib cmake --build .
