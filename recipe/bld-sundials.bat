be0b5d1c968cd70db04b06b0762755352439f003
Corey R. Randall <corey.r.randall@gmail.com>
2025-02-12 18:03:43 -0700

    Try with double backslashes

--- recipe/bld-sundials.bat
+++ recipe/bld-sundials.bat
@@ -1,6 +1,8 @@
 @echo ON
 
-cmake -LAH -G "Ninja" -B superlu_mt/build -S superlu_mt -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DPLAT="_OPENMP" -DBUILD_SHARED_LIBS=OFF
+set SAFE_PREFIX=%LIBRARY_PREFIX:\=\\%
+
+cmake -LAH -G "Ninja" -B superlu_mt/build -S superlu_mt -DCMAKE_INSTALL_PREFIX=%SAFE_PREFIX% -DPLAT="_OPENMP" -DBUILD_SHARED_LIBS=OFF
 if errorlevel 1 exit 1
 cmake --build superlu_mt/build --target install --config Release
 if errorlevel 1 exit 1
@@ -16,7 +18,7 @@ if /I "%PKG_NAME%" == "sundials" (
 )
 
 cmake -LAH -G "Ninja" -B sundials/build -S sundials ^
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX:\=/% ^
+    -DCMAKE_INSTALL_PREFIX=%SAFE_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DBUILD_SHARED_LIBS=%SUNDIALS_BUILD_SHARED% ^
     -DBUILD_STATIC_LIBS=%SUNDIALS_BUILD_STATIC% ^
@@ -27,17 +29,17 @@ cmake -LAH -G "Ninja" -B sundials/build -S sundials ^
     -DLAPACK_LIBRARIES="lapack;blas" ^
     -DCMAKE_C_FLAGS="/DWIN32 /D_WINDOWS /W3 /D__OPENMP" ^
     -DENABLE_KLU=ON ^
-    -DKLU_LIBRARY_DIR="%LIBRARY_PREFIX:\=/%/lib" ^
-    -DKLU_INCLUDE_DIR="%LIBRARY_PREFIX:\=/%/include/suitesparse" ^
-    -DKLU_LIBRARY="%LIBRARY_PREFIX:\=/%/lib/klu.lib" ^
-    -DAMD_LIBRARY="%LIBRARY_PREFIX:\=/%/lib/amd.lib" ^
-    -DBTF_LIBRARY="%LIBRARY_PREFIX:\=/%/lib/btf.lib" ^
-    -DCOLAMD_LIBRARY="%LIBRARY_PREFIX:\=/%/lib/colamd.lib" ^
+    -DKLU_SAFE_DIR="%SAFE_PREFIX%\\lib" ^
+    -DKLU_INCLUDE_DIR="%SAFE_PREFIX%\\include\\suitesparse" ^
+    -DKLU_LIBRARY="%SAFE_PREFIX%\\lib\\klu.lib" ^
+    -DAMD_LIBRARY="%SAFE_PREFIX%\\lib\\amd.lib" ^
+    -DBTF_LIBRARY="%SAFE_PREFIX%\\lib\\btf.lib" ^
+    -DCOLAMD_LIBRARY="%SAFE_PREFIX%\\lib\\colamd.lib" ^
     -DENABLE_SUPERLUMT=ON ^
     -DSUPERLUMT_THREAD_TYPE=OPENMP ^
     -DSUPERLUMT_LIBRARY="superlu_mt_OPENMP.lib" ^
-    -DSUPERLUMT_LIBRARIES="%LIBRARY_PREFIX:\=/%/lib/superlu_mt_OPENMP.lib;blas" ^
-    -DSUPERLUMT_INCLUDE_DIR="%LIBRARY_PREFIX:\=/%/include/superlu_mt" ^
+    -DSUPERLUMT_LIBRARIES="%SAFE_PREFIX%\\lib\\superlu_mt_OPENMP.lib;blas" ^
+    -DSUPERLUMT_INCLUDE_DIR="%SAFE_PREFIX%\\include\\superlu_mt" ^
     -DSUNDIALS_INDEX_SIZE=32
 if errorlevel 1 exit 1
 
