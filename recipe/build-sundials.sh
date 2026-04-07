9b9ae994526b87b9506d18c8911d43875624a0d9
Julien Schueller <schueller@phimeca.com>
2026-04-07 09:02:12 +0200

    Update to 7.7.0

--- recipe/build-sundials.sh
+++ recipe/build-sundials.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -euxo pipefail
 
-cmake ${CMAKE_ARGS} -LAH -G "Ninja" -B superlu_mt/build -S superlu_mt -DCMAKE_PREFIX_PATH=${PREFIX} -DPLAT="_OPENMP" -DBLA_VENDOR=Generic -DBUILD_SHARED_LIBS=OFF
+cmake ${CMAKE_ARGS} -LAH -G "Ninja" -B superlu_mt/build -S superlu_mt -DCMAKE_PREFIX_PATH=${PREFIX} -DPLAT="_OPENMP" -DBUILD_SHARED_LIBS=OFF
 cmake --build superlu_mt/build --target install --parallel ${CPU_COUNT}
 
 if [[ $PKG_NAME == "sundials-devel" || $PKG_NAME == "sundials" ]]; then
@@ -24,23 +24,22 @@ cmake ${CMAKE_ARGS} -LAH -G "Ninja" -B sundials/build -S sundials \
     -DBUILD_SHARED_LIBS=$SUNDIALS_BUILD_SHARED \
     -DBUILD_STATIC_LIBS=$SUNDIALS_BUILD_STATIC \
     -DCMAKE_C_STANDARD=11 \
-    -DEXAMPLES_ENABLE_C=ON \
-    -DEXAMPLES_INSTALL=OFF \
-    -DENABLE_OPENMP=ON \
-    -DENABLE_LAPACK=ON \
-    -DBLA_VENDOR=Generic \
+    -DSUNDIALS_ENABLE_C_EXAMPLES=ON \
+    -DSUNDIALS_ENABLE_EXAMPLES_INSTALL=OFF \
+    -DSUNDIALS_ENABLE_OPENMP=ON \
+    -DSUNDIALS_ENABLE_LAPACK=ON \
     -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" \
     -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
     -DCMAKE_MACOSX_RPATH=ON \
-    -DENABLE_KLU=ON \
+    -DSUNDIALS_ENABLE_KLU=ON \
     -DKLU_LIBRARY_DIR=${PREFIX}/lib \
     -DKLU_INCLUDE_DIR=${PREFIX}/include/suitesparse \
-    -DENABLE_SUPERLUMT=ON \
+    -DSUNDIALS_ENABLE_SUPERLUMT=ON \
     -DSUPERLUMT_THREAD_TYPE=OPENMP \
     -DSUPERLUMT_INCLUDE_DIR="${PREFIX}/include/superlu_mt" \
     -DSUPERLUMT_LIBRARY=libsuperlu_mt_OPENMP.a \
     -DSUPERLUMT_LIBRARIES="${PREFIX}/lib/libsuperlu_mt_OPENMP.a;blas" \
-    -DSUPERLUMT_WORKS=ON \
+    -DSUNDIALS_ENABLE_KLU_CHECKS=ON \
     -DSUNDIALS_INDEX_SIZE=32  # int32_t needed for Lapack not to be disabled
 
 cmake --build sundials/build --target install --parallel ${CPU_COUNT}
